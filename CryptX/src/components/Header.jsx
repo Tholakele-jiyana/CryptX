902aaf1cfa5bb744bae88944a3650576c57c70be
Tholakele-jiyana <tholakelejiyana@gmail.com>
2025-11-07 10:55:10 +0200

    Edited the placeholder text

--- CryptX/src/components/Header.jsx
+++ CryptX/src/components/Header.jsx
@@ -13,7 +13,7 @@ export default function Header() {
             <input 
               type="search" 
               className="w-full lg:w-64 px-4 py-2 border border-gray-300 rounded focus:outline-none focus:border-blue-500" 
-              placeholder="Search..." 
+              placeholder="Search type of keywords" 
               aria-label="Search"
             />
           </form>
