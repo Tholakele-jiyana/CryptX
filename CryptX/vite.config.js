c0f5600b01c8887d6edbac106a9e99b543e5e204
Tholakele-jiyana <tholakelejiyana@gmail.com>
2025-11-07 10:45:24 +0200

    Added/fixed the tailwind plugin configuration

--- CryptX/vite.config.js
+++ CryptX/vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
+// Added the tailwindcss plugin onto configuration
+
 export default defineConfig({
   plugins: [
     react({
@@ -9,5 +12,6 @@ export default defineConfig({
         plugins: [['babel-plugin-react-compiler']],
       },
     }),
+    tailwindcss(),  
   ],
 })
