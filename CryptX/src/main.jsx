eb0eb408eb2dc0bdcfdd3696b544275a88744232
Tholakele-jiyana <tholakelejiyana@gmail.com>
2025-11-09 23:59:12 +0200

    Updated the configuration on the pages so that they could render

--- CryptX/src/main.jsx
+++ CryptX/src/main.jsx
@@ -1,8 +1,9 @@
 //Configuring the main.jsx file to include the index.css file for styling
 
 import React from 'react';
-import App from './App.jsx';
 import ReactDOM from "react-dom/client";
+import App from './App.jsx';
+import './index.css';
 
 const root=ReactDOM.createRoot(document.getElementById("root")).render(<React.StrictMode>
   <App/>
