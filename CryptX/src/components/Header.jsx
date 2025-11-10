5f9a26e30bd0ce5a1edbb72362eed1d390819a65
Tholakele-jiyana <tholakelejiyana@gmail.com>
2025-11-10 11:56:12 +0200

    Removed the import staement and updated the img src

--- CryptX/src/components/Header.jsx
+++ CryptX/src/components/Header.jsx
@@ -1,13 +1,10 @@
 //Render React function 'Header' component for the application
 
- // Added import statement because image does not render after deployment
-// Corrected the import name to avoid conflict with the component name
-
 function Header() {
   return (
     <header style= {{position: 'absolute', top: '32px', left: '302px', width: '1098px', height: '73px'}} >
         <img 
-          src="/assets/Header.png" 
+          src="/assets/Header.png" // moved the image within assest folder from src folder to public folder
           alt="Header" 
           style={{ width: '100%', height: '100%' }}
           className="object-cover"
